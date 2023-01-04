f0a8e43f18fc3d4e21914aca122f1ea900e20cf4
Veysel Artuc <mva.artuc@gmail.com>
2023-01-04 11:26:36 +0300

    added ClubController and AppUserController

--- SoleMates/Controllers/ClubController.cs
+++ SoleMates/Controllers/ClubController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SoleMates.Data;
 
 namespace SoleMates.Controllers
 {
     public class ClubController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var clubs = _context.Clubs.ToList();
+            return View(clubs);
         }
     }
 }
