7cc1325f1e71b9826d38bee85feb285b84db270c
Veysel Artuc <mva.artuc@gmail.com>
2023-01-05 01:43:28 +0300

    Added update functionality on club/race

--- SoleMates/Controllers/RaceController.cs
+++ SoleMates/Controllers/RaceController.cs
@@ -59,5 +59,63 @@ namespace SoleMates.Controllers
                 return View(raceViewModel);
             }
         }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var race = await _raceRepository.GetByIdAsync(id);
+            if (race == null) return View("Error");
+            var raceViewModel = new EditRaceViewModel
+            {
+                Title = race.Title,
+                Description = race.Description,
+                AddressId = race.AddressId,
+                Address = race.Address,
+                ImageUrl = race.Image,
+                RaceCategory = race.RaceCategory
+            };
+            return View(raceViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, EditRaceViewModel raceViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Failed to edit race");
+                return View("Edit", raceViewModel);
+            }
+            var userRace = await _raceRepository.GetByIdNoTrackingAsync(id);
+
+            if (userRace != null)
+            {
+                try
+                {
+                    await _photoService.DeletePhotoAsync(userRace.Image);
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "Could not delete photo");
+                    return View(raceViewModel);
+                }
+                var photoResult = await _photoService.AddPhotoAsync(raceViewModel.Image);
+                var race = new Race
+                {
+                    Id = id,
+                    Title = raceViewModel.Title,
+                    Description = raceViewModel.Description,
+                    Image = photoResult.Url.ToString(),
+                    AddressId = raceViewModel.AddressId,
+                    Address = raceViewModel.Address
+                };
+
+                _raceRepository.Update(race);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(raceViewModel);
+            }
+        }
+
     }
 }
