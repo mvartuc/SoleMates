b3257a1acca05b2f3d77ac247cb420e5eedeb681
Veysel Artuc <mva.artuc@gmail.com>
2023-01-04 23:19:55 +0300

    Integrated Cloudinary to the project to store images more efficiently in the cloud Injected dependencies for Cloudinary and IPhotoService

--- SoleMates/Program.cs
+++ SoleMates/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using SoleMates.Data;
+using SoleMates.Helpers;
 using SoleMates.Interfaces;
 using SoleMates.Repository;
+using SoleMates.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IClubRepository, ClubRepository>();
 builder.Services.AddScoped<IRaceRepository, RaceRepository>();
+builder.Services.AddScoped<IPhotoService, PhotoService>();
+builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
