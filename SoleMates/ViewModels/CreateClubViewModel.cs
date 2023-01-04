3ca24496be60dbc319947634224afc96cd2f5314
Veysel Artuc <mva.artuc@gmail.com>
2023-01-04 23:20:40 +0300

    Added ViewModels for creating a club/race

---
+++ SoleMates/ViewModels/CreateClubViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SoleMates.Data.Enum;
+using SoleMates.Models;
+
+namespace SoleMates.ViewModels
+{
+    public class CreateClubViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Address Address { get; set; }
+        public IFormFile Image { get; set; }
+        public ClubCategory ClubCategory { get; set; }
+
+    }
+}
---
+++ SoleMates/ViewModels/CreateRaceViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SoleMates.Data.Enum;
+using SoleMates.Models;
+
+namespace SoleMates.ViewModels
+{
+    public class CreateRaceViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Address Address { get; set; }
+        public IFormFile Image { get; set; }
+        public RaceCategory RaceCategory { get; set; }
+
+    }
+}
