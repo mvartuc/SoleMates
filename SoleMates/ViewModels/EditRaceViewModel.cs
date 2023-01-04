9e58ca933083fc5169b67df3a5ebfb559ec905d0
Veysel Artuc <mva.artuc@gmail.com>
2023-01-05 01:44:24 +0300

    Added edit view models

---
+++ SoleMates/ViewModels/EditRaceViewModel.cs
@@ -0,0 +1,17 @@
+﻿using SoleMates.Data.Enum;
+using SoleMates.Models;
+
+namespace SoleMates.ViewModels
+{
+    public class EditRaceViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public IFormFile Image { get; set; }
+        public string? ImageUrl { get; set; }
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
+        public RaceCategory RaceCategory { get; set; }
+    }
+}
