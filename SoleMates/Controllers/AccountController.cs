d7ffb98e7b9da7ece69e8c4d6d72475ca3540642
Veysel Artuc <mva.artuc@gmail.com>
2023-01-07 12:23:31 +0300

    Added Login functionality with a login page

---
+++ SoleMates/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SoleMates.Data;
+using SoleMates.Models;
+using SoleMates.ViewModels;
+
+namespace SoleMates.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(loginViewModel);
+            }
+            var user = await _userManager.FindByEmailAsync(loginViewModel.EmailAddress);
+
+            // user found
+            if (user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
+                if(passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Race");
+                    }
+                }
+                TempData["Error"] = "Wrong Password";
+                return View(loginViewModel);
+            }
+            // user not found
+            TempData["Error"] = "Wrong credentials, Please try again!";
+            return View(loginViewModel);
+        }
+    }
+}
---
+++ SoleMates/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoleMates.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email is required!")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Enter a valid e-mail!")]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Password is required!")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
