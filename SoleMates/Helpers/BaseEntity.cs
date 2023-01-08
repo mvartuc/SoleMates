375be378187d6e405d6d7cd2411bf94ba847e9a9
Veysel Artuc <mva.artuc@gmail.com>
2023-01-08 04:03:27 +0300

    Polymorphism for Date fields

---
+++ SoleMates/Helpers/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace SoleMates.Helpers
+{
+    public class BaseEntity
+    {
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateModified { get; set; }
+        public TimeSpan? Duration { get; set; }
+    }
+}
